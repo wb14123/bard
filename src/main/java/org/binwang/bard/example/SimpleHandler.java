8f0f89d49f4f8386de12463996ad9b8271f9a172
Bin Wang <wbin00@gmail.com>
2014-09-22 18:05:03 +0800

    CURD example

--- src/main/java/org/binwang/bard/example/SimpleHandler.java
+++ src/main/java/org/binwang/bard/example/SimpleHandler.java
@@ -1,5 +1,6 @@
 package org.binwang.bard.example;
 
+import org.binwang.bard.basic.marker.Doc;
 import org.binwang.bard.basic.marker.Required;
 import org.binwang.bard.core.Handler;
 
@@ -15,17 +16,43 @@ public class SimpleHandler extends Handler {
 
     @GET
     @Path("/{id}")
+    @Doc("Get a user by id")
     public User getUser(@PathParam("id") @Required int id) {
         return UserStorage.get(id);
     }
 
     @PUT
+    @Doc("Create a new user")
     public User insertUser(
         @FormParam("id") @Required int id,
         @FormParam("name") String name) {
         User user = new User();
         user.id = id;
         user.name = name;
+        UserStorage.put(id, user);
+        return user;
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Doc("Delete a user")
+    public String deleteUser(@PathParam("id") @Required int id) {
+        UserStorage.remove(id);
+        return "ok";
+    }
+
+    @POST
+    @Doc("Update a user")
+    public User updateUser(
+        @FormParam("id") @Required int id,
+        @FormParam("name") String name) {
+        User user = UserStorage.get(id);
+        if (user == null) {
+            return null;
+        }
+        if (name != null) {
+            user.name = name;
+        }
         UserStorage.put(id, user);
         return user;
     }
