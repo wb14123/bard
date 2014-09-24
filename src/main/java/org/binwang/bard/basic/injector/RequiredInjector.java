c69702a202139d81da9197d32cbd793f9dfab216
Bin Wang <wbin00@gmail.com>
2014-09-24 11:49:04 +0800

    use HandleError with Required

--- src/main/java/org/binwang/bard/basic/injector/RequiredInjector.java
+++ src/main/java/org/binwang/bard/basic/injector/RequiredInjector.java
@@ -1,25 +1,22 @@
 package org.binwang.bard.basic.injector;
 
+import org.binwang.bard.basic.marker.ErrorCase;
+import org.binwang.bard.basic.marker.HandleErrors;
 import org.binwang.bard.basic.marker.Required;
 import org.binwang.bard.core.BindTo;
 import org.binwang.bard.core.Injector;
-import org.binwang.bard.core.marker.After;
 import org.binwang.bard.core.marker.Before;
 
 @BindTo(Required.class)
 public class RequiredInjector extends Injector<Required> {
     @Before
+    @HandleErrors({
+        @ErrorCase(description = "required param don't in",
+            code = 10001, exception = RequiredNullException.class, logLevel = "DEBUG")
+    })
     public void validateParam() throws RequiredNullException {
         if (injectorVariable == null) {
             throw new RequiredNullException();
-        }
-    }
-
-    @After
-    public void handleNullRequired() {
-        if (context.exception instanceof RequiredNullException) {
-            context.exception = null;
-            context.result = "should not be null";
         }
     }
 
