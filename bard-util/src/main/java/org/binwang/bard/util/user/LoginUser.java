6186c5ab59995fec7f4ec3502cdab224cfc717c2
Bin Wang <wbin00@gmail.com>
2014-10-04 08:10:51 +0800

    fix annotation define

--- bard-util/src/main/java/org/binwang/bard/util/user/LoginUser.java
+++ bard-util/src/main/java/org/binwang/bard/util/user/LoginUser.java
@@ -1,4 +1,8 @@
 package org.binwang.bard.util.user;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface LoginUser {
 }
