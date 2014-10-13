fd249962d93ee9ecefb6f851c65c03387316eef5
Bin Wang <wbin00@gmail.com>
2014-10-13 18:17:41 +0800

    password encrypter

---
+++ bard-util/src/main/java/org/binwang/bard/util/user/PasswordEncrypter.java
@@ -0,0 +1,33 @@
+package org.binwang.bard.util.user;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+public class PasswordEncrypter {
+    /**
+     * Encrypt a password string with a random generated salt.
+     *
+     * @param password The origin password need to be encrypt.
+     * @return An array with two elements: The first one is the encrypted password and the second one is the salt.
+     */
+    public static String[] encrypt(String password) {
+        SecureRandom random = new SecureRandom();
+        String salt = new BigInteger(130, random).toString(32);
+        String encryptPassword = encrypt(password, salt);
+        return new String[]{salt, encryptPassword};
+    }
+
+    /**
+     * Encrypt a password string with a given password.
+     *
+     * @param password The origin password need to be encrypt.
+     * @param salt The salt.
+     * @return The encrypted password.
+     */
+    public static String encrypt(String password, String salt) {
+        String saltPassword = password + salt;
+        return DigestUtils.sha256Hex(saltPassword.getBytes());
+    }
+}
