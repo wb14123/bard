97b33698655c0cf6aa67a40df7a41fab0e4c76ee
Bin Wang <wbin00@gmail.com>
2014-10-13 22:13:49 +0800

    fix hibernate config

--- bard-util/src/main/java/org/binwang/bard/util/db/DBManager.java
+++ bard-util/src/main/java/org/binwang/bard/util/db/DBManager.java
@@ -11,15 +11,15 @@ public class DBManager {
     public static void init(String[] pkgs, Class<?>[] classes) {
         try {
             // Create the SessionFactory from hibernate.cfg.xml
-            Configuration cfg = new Configuration().configure();
+            Configuration cfg = new Configuration();
             for (String pkg : pkgs) {
                 cfg = cfg.addPackage(pkg);
             }
             for (Class<?> c : classes) {
                 cfg = cfg.addAnnotatedClass(c);
             }
-            sessionFactory = cfg.buildSessionFactory(
-                new StandardServiceRegistryBuilder().build());
+            sessionFactory = cfg.configure().buildSessionFactory(
+                new StandardServiceRegistryBuilder().applySettings(cfg.getProperties()).build());
         } catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed
             System.err.println("Initial SessionFactory creation failed." + ex);
