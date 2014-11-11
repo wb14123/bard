2cefcf17a26479d736e7c500e8a8dd46a0ede43e
Bin Wang <wbin00@gmail.com>
2014-11-11 16:19:15 +0800

    fix bard:war plugin, package dependency libraries into War package

--- bard-maven-plugin/src/main/java/com/bardframework/plugin/BardMojo.java
+++ bard-maven-plugin/src/main/java/com/bardframework/plugin/BardMojo.java
@@ -5,10 +5,7 @@ import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.BuildPluginManager;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugins.annotations.Component;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.*;
 import org.apache.maven.project.MavenProject;
 
 import java.io.File;
@@ -17,7 +14,8 @@ import java.io.IOException;
 
 import static org.twdata.maven.mojoexecutor.MojoExecutor.*;
 
-@Mojo(name = "war", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
+@Mojo(name = "war", defaultPhase = LifecyclePhase.PACKAGE,
+    requiresDependencyResolution = ResolutionScope.RUNTIME)
 public class BardMojo extends AbstractMojo {
 
     File webAppFile;
