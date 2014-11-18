285d285a5000eb52047ee5f38d6369210543586e
Bin Wang <wbin00@gmail.com>
2014-11-18 18:18:14 +0800

    delete bard-example, since bard-archetype is already a good example

--- bard-example/src/main/java/com/bardframework/bard/example/crud/Main.java
+++
@@ -1,9 +0,0 @@
-package com.bardframework.bard.example.crud;
-
-import com.bardframework.bard.util.server.BardServer;
-
-public class Main {
-    public static void main(String[] args) {
-        new BardServer(CrudServlet.class).run();
-    }
-}
--- bard-example/src/main/java/com/bardframework/bard/example/db/Main.java
+++
@@ -1,9 +0,0 @@
-package com.bardframework.bard.example.db;
-
-import com.bardframework.bard.util.server.BardServer;
-
-public class Main {
-    public static void main(String[] args) {
-        new BardServer(SimpleServlet.class).run();
-    }
-}
