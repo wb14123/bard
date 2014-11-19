41c42441d4515801418703cfbfc1c2a697b8a0e6
Bin Wang <wbin00@gmail.com>
2014-11-19 11:53:04 +0800

    init EntityManger in method so that it is easier to debug

--- bard-util/src/main/java/com/bardframework/bard/util/db/DBManager.java
+++ bard-util/src/main/java/com/bardframework/bard/util/db/DBManager.java
@@ -7,12 +7,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
 public class DBManager {
-    private static EntityManager em =
-        Persistence.createEntityManagerFactory(
-            Util.getConfig().getString("bard.jpa.unit", "prod")
-        ).createEntityManager();
+    private static EntityManager em = null;
 
     public static EntityManager getEntityManager() {
+        if (em == null) {
+            em = Persistence.createEntityManagerFactory(
+                Util.getConfig().getString("bard.jpa.unit", "prod")
+            ).createEntityManager();
+        }
         return em;
     }
 }
