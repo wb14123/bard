e316d0fabc2a73f4f7564fc2d99c6d315ac2370a
Bin Wang <wbin00@gmail.com>
2015-02-12 16:57:49 +0800

    move archetypes into split directory

--- archetype.sh
+++ archetype.sh
@@ -5,23 +5,32 @@ MAVEN_GOAL=$1
 
 mvn clean
 
-mvn $MAVEN_GOAL || exit 1
+mvn ${MAVEN_GOAL} || exit 1
+
 cd bard-archetype
-mvn archetype:create-from-project -Darchetype.keepParent=false
-cd target/generated-sources/archetype
 
-SED_POM_CMD="s/<groupId>com.bardframework<\/groupId>/<parent><groupId>com.bardframework<\/groupId><artifactId>bard<\/artifactId><version>$VERSION<\/version><\/parent>/g"
-SED_ARCHETYPE_POM_CMD="s/org.apache.saltedpeanuts/\${package}/g"
-SED_JAVA_CMD="s/\${groupId}/com.bardframework/g"
+# do things for every sub-directory about archetype
+for DIR in `ls -d bard-*/`; do
 
-if [ `uname` = 'Linux' ]; then
-	find ./src -type f ! -name "pom.xml" | xargs sed -i "$SED_JAVA_CMD"
-	sed -i "$SED_POM_CMD" pom.xml
-	sed -i "$SED_ARCHETYPE_POM_CMD" src/main/resources/archetype-resources/pom.xml
-else
-	find ./src -type f ! -name "pom.xml" | xargs sed -i '' "$SED_JAVA_CMD"
-	sed -i '' "$SED_POM_CMD" pom.xml
-	sed -i '' "$SED_ARCHETYPE_POM_CMD" src/main/resources/archetype-resources/pom.xml
-fi
+    cd ${DIR}
+    mvn archetype:create-from-project -Darchetype.keepParent=false
+    cd target/generated-sources/archetype
 
-mvn $MAVEN_GOAL
+    SED_POM_CMD="s/<groupId>com.bardframework<\/groupId>/<parent><groupId>com.bardframework<\/groupId><artifactId>bard<\/artifactId><version>$VERSION<\/version><\/parent>/g"
+    SED_ARCHETYPE_POM_CMD="s/org.apache.saltedpeanuts/\${package}/g"
+    SED_JAVA_CMD="s/\${groupId}/com.bardframework/g"
+
+    if [ `uname` = 'Linux' ]; then
+    	find ./src -type f ! -name "pom.xml" | xargs sed -i "$SED_JAVA_CMD"
+    	sed -i "$SED_POM_CMD" pom.xml
+    	sed -i "$SED_ARCHETYPE_POM_CMD" src/main/resources/archetype-resources/pom.xml
+    else
+    	find ./src -type f ! -name "pom.xml" | xargs sed -i '' "$SED_JAVA_CMD"
+    	sed -i '' "$SED_POM_CMD" pom.xml
+    	sed -i '' "$SED_ARCHETYPE_POM_CMD" src/main/resources/archetype-resources/pom.xml
+    fi
+
+    mvn ${MAVEN_GOAL}
+    cd ..
+
+done
